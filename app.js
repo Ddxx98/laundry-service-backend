4014bf9d661639310b3f7a8a651facf1251c419b
meghna singh <megsingh2212@gmail.com>
2022-03-19 12:59:40 +0530

    setup app.js and user schema

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+var cors = require('cors');
+const PORT = process.env.PORT || 5000
+SECRET = "RESTAPI"
+
+const app = express(); 
+
+mongoose.connect('mongodb+srv://meghna:11223344@cluster0.wpyvn.mongodb.net/laundryService?retryWrites=true&w=majority')
+
+app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.listen(PORT,()=>{  // bind the connections on this port and listen to it
+    console.log(`example app listening on port ${PORT}`);
+})
