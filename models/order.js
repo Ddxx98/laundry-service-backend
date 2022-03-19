9d15c93b125b4332b344c8a519bed29bf9a7087a
meghna singh <megsingh2212@gmail.com>
2022-03-19 17:43:03 +0530

    updated order and user schema

--- models/order.js
+++ models/order.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose
 
+const productSchema = new Schema({
+    productType: { type: String },
+    quantity: { type: Number, default: 0 },
+    washing: { type: Boolean, default: false },
+    ironing: { type: Boolean, default: false },
+    drywash: { type: Boolean, default: false },
+    chemicalwash: { type: Boolean, default: false },
+  })
+  
 const orderSchema = new Schema({
     user: {type: mongoose.Types.ObjectId ,ref:'User'},
-    product: {type: [{type: Map, of: String}]}
+    product: {type: [productSchema]}
 })
 
 const Order = mongoose.model('Order',orderSchema)
