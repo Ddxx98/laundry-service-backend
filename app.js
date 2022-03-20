7347c410aa8504bd570cd89074ef841af9358a06
meghna singh <megsingh2212@gmail.com>
2022-03-20 11:58:56 +0530

    added authorization code in app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 var cors = require('cors');
+const jwt = require('jsonwebtoken');
+SECRET = "RESTAPI" 
 
 const app = express(); 
 
@@ -19,11 +21,35 @@ const orderRoutes = require("./routes/orders")
 
 app.use("/api/v1",loginRoutes)
 app.use("/ap1/v1",registerRoutes)
-app.use("/api/v1/orders",orderRoutes)
+app.use("/api/v1/",orderRoutes)
 
 /* =================== MONGOOSE CONNECTION =======================*/
 mongoose.connect('mongodb+srv://meghna:11223344@cluster0.wpyvn.mongodb.net/laundryService?retryWrites=true&w=majority')
 const PORT = process.env.PORT || 5000
+
+/* ==================== AUTHORIZATION ============================*/
+app.use("/orders",(req,res,next)=>{
+
+  var token = req.headers.authorization.split("Bearer ")[1];
+  if(!token){
+      return res.status(401).json({
+          status:"failed",
+          message:"token is missing"
+      })
+  }
+  jwt.verify(token,SECRET,function(err,decoded){  // jwt keeps a record of the tokens
+      if(err){
+          return res.status(401).json({
+              status:"failed",
+              message:"invalid token"
+          })
+      }
+      else{
+          req.user = decoded.data
+          next();
+      }
+  })
+})
 
 /*===================== START SERVER ==============================*/
 app.listen(PORT,()=>{  
